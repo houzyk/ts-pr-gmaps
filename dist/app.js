dd3f834b5e845e7b0211b4dd7fd0d9cba507e84d
houzyk <houzairmk@icloud.com>
2022-01-20 13:30:33 +0400

    ADD google map display

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,17 @@ function handleSearch(e) {
     e.preventDefault();
     const enteredAddress = formInput.value;
     axios_1.default.get(`${encodeURI(enteredAddress)}${apiKey}`)
-        .then()
+        .then(response => {
+        var _a;
+        if (response.data.status !== 'OK')
+            throw new Error('');
+        const coordinates = (_a = response.data) === null || _a === void 0 ? void 0 : _a.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: coordinates,
+            zoom: 16
+        });
+        new google.maps.Marker({ position: coordinates, map: map });
+    })
         .catch(err => console.error(err));
 }
 addressForm.addEventListener('submit', handleSearch);
--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,13 @@ const formInput = document.getElementById('address')! as HTMLInputElement;
 
 const apiKey = '';
 
+declare const google: any;
+
 function handleSearch (e:Event) {
   e.preventDefault();
   const enteredAddress = formInput.value;
 
-  // api
+  // api call for cd
   type googleAPI = {
     results: {geometry: { location: {lat: number, long: number}}}[];
     status: 'OK' | 'ZERO_RESULTS';
@@ -20,6 +22,14 @@ function handleSearch (e:Event) {
         .then( response => {
           if (response.data.status !== 'OK') throw new Error('');
           const coordinates = response.data?.results[0].geometry.location;
+
+          // map display
+          const map = new google.maps.Map(document.getElementById("map")!, {
+            center: coordinates,
+            zoom: 16
+          }); 
+          new google.maps.Marker({ position: coordinates, map: map });
+
         })
         .catch(err => console.error(err));
 }
