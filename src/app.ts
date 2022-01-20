c1285b93b4bcf2435e820a12d1c7c6be7a1c56f9
houzyk <houzairmk@icloud.com>
2022-01-20 13:08:49 +0400

    ADD axios npm

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 const addressForm = document.querySelector('form')! as HTMLFormElement;
 const formInput = document.getElementById('address')! as HTMLInputElement;
 
+const apiKey = '';
+
 function handleSearch (e:Event) {
   e.preventDefault();
   const enteredAddress = formInput.value;
 
   // api
+  
 }
 
 addressForm.addEventListener('submit', handleSearch);
